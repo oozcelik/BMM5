39a6f4224121240c608e6238fc5d5ea5f8cd6962
Ozlem Ozcelik <ozlem.ozcelik@cern.ch>
2019-11-04 17:18:31 +0300

    added PU-reweighting

--- Bmm/CmsswAnalysis/plugins/HFDumpStuff.h
+++ Bmm/CmsswAnalysis/plugins/HFDumpStuff.h
@@ -11,6 +11,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
+#include "PhysicsTools/Utilities/interface/LumiReWeighting.h"
+#include "PhysicsTools/Utilities/interface/LumiReweightingStandAlone.h"
 
 #include "DataFormats/Luminosity/interface/LumiSummary.h"
 #include "DataFormats/Luminosity/interface/LumiDetails.h"
@@ -32,18 +35,22 @@ class HFDumpStuff : public edm::EDAnalyzer {
   ~HFDumpStuff();
   
  private:
+  edm::LumiReWeighting lumi_weights;
   virtual void beginJob() ;
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
-
+  
+//  edm::LumiReWeighting lumi_weights;
   int                  fVerbose; 
   std::string	       fCandidates1Label, fCandidates2Label, fCandidates3Label;
-  edm::InputTag        fLumiSummaryLabel, fBeamSpotLabel, fPrimaryVertexLabel, fPrimaryVertexTracksLabel; 
-
+  edm::InputTag        fLumiSummaryLabel, fBeamSpotLabel, fPrimaryVertexLabel, fPrimaryVertexTracksLabel, fPileUpInfo; 
+//  edm::EDGetTokenT<std::vector<PileupSummaryInfo>>fPileUp;
+  std::string pileup_path_;
   edm::EDGetTokenT<LumiSummary> fTokenLumiSummary;
   edm::EDGetTokenT<reco::BeamSpot> fTokenBeamSpot;
   edm::EDGetTokenT<std::vector<reco::Track> > fTokenTrack;
   edm::EDGetTokenT<reco::VertexCollection> fTokenVertex;
+  edm::EDGetTokenT<std::vector<PileupSummaryInfo>>fPileUp;
   
 };
 
