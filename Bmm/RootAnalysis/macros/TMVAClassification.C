38152b312fba60b747fba21730d759c267fa81f1
Ozlem Ozcelik <ozlem.ozcelik@cern.ch>
2019-12-17 12:58:11 +0300

    for muon BDT classification

---
+++ Bmm/RootAnalysis/macros/TMVAClassification.C
@@ -0,0 +1,343 @@
+/// \file
+/// \ingroup tutorial_tmva
+/// \notebook -nodraw
+/// This macro provides examples for the training and testing of the
+/// TMVA classifiers.
+///
+/// As input data is used a toy-MC sample consisting of four Gaussian-distributed
+/// and linearly correlated input variables.
+/// The methods to be used can be switched on and off by means of booleans, or
+/// via the prompt command, for example:
+///
+///     root -l ./TMVAClassification.C\(\"Fisher,Likelihood\"\)
+///
+/// (note that the backslashes are mandatory)
+/// If no method given, a default set of classifiers is used.
+/// The output file "TMVA.root" can be analysed with the use of dedicated
+/// macros (simply say: root -l <macro.C>), which can be conveniently
+/// invoked through a GUI that will appear at the end of the run of this macro.
+/// Launch the GUI via the command:
+///
+///     root -l ./TMVAGui.C
+///
+/// You can also compile and run the example with the following commands
+///
+///     make
+///     ./TMVAClassification <Methods>
+///
+/// where: `<Methods> = "method1 method2"` are the TMVA classifier names
+/// example:
+///
+///     ./TMVAClassification Fisher LikelihoodPCA BDT
+///
+/// If no method given, a default set is of classifiers is used
+///
+/// - Project   : TMVA - a ROOT-integrated toolkit for multivariate data analysis
+/// - Package   : TMVA
+/// - Root Macro: TMVAClassification
+///
+/// \macro_output
+/// \macro_code
+/// \author Andreas Hoecker
+
+
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+
+#include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/Tools.h"
+#include "TMVA/TMVAGui.h"
+
+int TMVAClassification( TString myMethodList = "" )
+{
+   // The explicit loading of the shared libTMVA is done in TMVAlogon.C, defined in .rootrc
+   // if you use your private .rootrc, or run from a different directory, please copy the
+   // corresponding lines from .rootrc
+
+   // Methods to be processed can be given as an argument; use format:
+   //
+   //     mylinux~> root -l TMVAClassification.C\(\"myMethod1,myMethod2,myMethod3\"\)
+
+   //---------------------------------------------------------------
+   // This loads the library
+   TMVA::Tools::Instance();
+
+   // Default MVA methods to be trained + tested
+   std::map<std::string,int> Use;
+
+   // Boosted Decision Trees
+   Use["BDT"]             = 1; // uses Adaptive Boost
+   Use["BDTG"]            = 0; // uses Gradient Boost
+   Use["BDTB"]            = 0; // uses Bagging
+   // ---------------------------------------------------------------
+
+   std::cout << std::endl;
+   std::cout << "==> Start TMVAClassification" << std::endl;
+
+   // Select methods (don't look at this code - not of interest)
+   if (myMethodList != "") {
+      for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) it->second = 0;
+
+      std::vector<TString> mlist = TMVA::gTools().SplitString( myMethodList, ',' );
+      for (UInt_t i=0; i<mlist.size(); i++) {
+         std::string regMethod(mlist[i]);
+
+         if (Use.find(regMethod) == Use.end()) {
+            std::cout << "Method \"" << regMethod << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
+            for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) std::cout << it->first << " ";
+            std::cout << std::endl;
+            return 1;
+         }
+         Use[regMethod] = 1;
+      }
+   }
+
+   // --------------------------------------------------------------------------------------------------
+
+   // Here the preparation phase begins
+
+   // Read training and test data
+   // (it is also possible to use ASCII format as input -> see TMVA Users Guide)
+
+   TFile *bckinput(0);
+   TString fname = "output.root";
+   bckinput = TFile::Open( fname );
+
+   TFile *siginput(0);
+   TString fname2 = "output.bmmReader.mix-Bs2MuMu.root"; 
+   siginput = TFile::Open( fname ); // check if file in local directory exists
+
+   if (!siginput || !bckinput) {
+      std::cout << "ERROR: could not open data file" << std::endl;
+      exit(1);
+   }
+   std::cout << "--- TMVAClassification       : Using signal input file: " << siginput->GetName() << std::endl;
+   std::cout << "--- TMVAClassification       : Using background input file: " << bckinput->GetName() << std::endl;
+
+   // Register the training and test trees
+
+   TTree *signalTree     = (TTree*)siginput->Get("candAnaMuMu/muonidtree");
+   TTree *background     = (TTree*)bckinput->Get("candAnaMuMu/muonidtree");
+
+   TTree *signalTree2     = (TTree*)siginput->Get("candAnaMuMu/events");
+   TTree *background2     = (TTree*)bckinput->Get("candAnaMuMu/events");
+
+   // Create a ROOT output file where TMVA will store ntuples, histograms, etc.
+   TString outfileName( "TMVA.root" );
+   TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
+
+   // Create the factory object. Later you can choose the methods
+   // whose performance you'd like to investigate. The factory is
+   // the only TMVA object you have to interact with
+   //
+   // The first argument is the base of the name of all the
+   // weightfiles in the directory weight/
+   //
+   // The second argument is the output file for the training results
+   // All TMVA output can be suppressed by removing the "!" (not) in
+   // front of the "Silent" argument in the option string
+   TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile,
+                                               "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
+
+   TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
+   // If you wish to modify default settings
+   // (please check "src/Config.h" to see all available global options)
+   //
+   //    (TMVA::gConfig().GetVariablePlotting()).fTimesRMS = 8.0;
+   //    (TMVA::gConfig().GetIONames()).fWeightFileDir = "myWeightDirectory";
+
+   // Define the input variables that shall be used for the MVA training
+   // note that you may also use variable expressions, such as: "3*var1/var2*abs(var3)"
+   // [all types of expressions that can also be parsed by TTree::Draw( "expression" )]
+
+     dataloader->AddVariable( "pt", 'F' );
+     dataloader->AddVariable( "eta", 'F' );
+//     dataloader->AddVariable( "Qprod", 'I' );
+//     dataloader->AddVariable( "vMuonHitComb", 'F' );
+ //    dataloader->AddVariable( "trkRelChi2", 'F' );
+//     dataloader->AddVariable( "TMTrkMult100", 'F' );
+/*     dataloader->AddVariable( "valPixHits", 'F' );
+     dataloader->AddVariable( "innerChi2", 'F' );
+     dataloader->AddVariable( "outerChi2", 'F' );
+     dataloader->AddVariable( "timeAtIpInOutErr", 'F' );
+     dataloader->AddVariable( "glbKinkFinder", 'F' );
+     dataloader->AddVariable( "kinkFinder", 'F' );
+//     dataloader->AddVariable( "dzRef", 'F' );
+//     dataloader->AddVariable( "dxyRef", 'F' );
+     dataloader->AddVariable( "LWH", 'F' );
+//     dataloader->AddVariable( "dr", 'F' );
+//     dataloader->AddVariable( "glbTrackProb", 'F' );
+     dataloader->AddVariable( "trkValidFract", 'F' );
+     dataloader->AddVariable( "NTrkVHits", 'F' );
+     dataloader->AddVariable( "chi2LocPos", 'F' );
+     dataloader->AddVariable( "chi2LocMom", 'F' );
+     dataloader->AddVariable( "segComp", 'F' );*/
+ //    dataloader->AddVariable( "gmuid", 'I');
+ //    dataloader->AddVariable( "gmu1gmid", 'F');
+//     dataloader->AddVariable( "gmu2gmid", 'F');
+
+   // You can add so-called "Spectator variables", which are not used in the MVA training,
+   // but will appear in the final "TestTree" produced by TMVA. This TestTree will contain the
+   // input variables, the response values of all trained MVAs, and the spectator variables
+
+//   dataloader->AddSpectator( "spec1 := var1*2",  "Spectator 1", "units", 'F' );
+//   dataloader->AddSpectator( "spec2 := var1*3",  "Spectator 2", "units", 'F' );
+
+   // global event weights per tree (see below for setting event-wise weights)
+   Double_t signalWeight     = 1.0;
+   Double_t backgroundWeight = 1.0;
+
+   // You can add an arbitrary number of signal or background trees
+   dataloader->AddSignalTree    ( signalTree, signalWeight );
+   dataloader->AddBackgroundTree( background, backgroundWeight );
+
+   // To give different trees for training and testing, do as follows:
+   //
+//   dataloader->AddSignalTree( signalTrainingTree, signalTrainWeight, "Training" );
+//   dataloader->AddSignalTree( signalTestTree,     signalTestWeight,  "Test" );
+
+   // Use the following code instead of the above two or four lines to add signal and background
+   // training and test events "by hand"
+   // NOTE that in this case one should not give expressions (such as "var1+var2") in the input
+   //      variable definition, but simply compute the expression before adding the event
+   // ```cpp
+   // // --- begin ----------------------------------------------------------
+   // std::vector<Double_t> vars( 4 ); // vector has size of number of input variables
+   // Float_t  treevars[4], weight;
+   //
+   // // Signal
+   // for (UInt_t ivar=0; ivar<4; ivar++) signalTree->SetBranchAddress( Form( "var%i", ivar+1 ), &(treevars[ivar]) );
+   // for (UInt_t i=0; i<signalTree->GetEntries(); i++) {
+   //    signalTree->GetEntry(i);
+   //    for (UInt_t ivar=0; ivar<4; ivar++) vars[ivar] = treevars[ivar];
+   //    // add training and test events; here: first half is training, second is testing
+   //    // note that the weight can also be event-wise
+   //    if (i < signalTree->GetEntries()/2.0) dataloader->AddSignalTrainingEvent( vars, signalWeight );
+   //    else                              dataloader->AddSignalTestEvent    ( vars, signalWeight );
+   // }
+   //
+   // // Background (has event weights)
+   // background->SetBranchAddress( "weight", &weight );
+   // for (UInt_t ivar=0; ivar<4; ivar++) background->SetBranchAddress( Form( "var%i", ivar+1 ), &(treevars[ivar]) );
+   // for (UInt_t i=0; i<background->GetEntries(); i++) {
+   //    background->GetEntry(i);
+   //    for (UInt_t ivar=0; ivar<4; ivar++) vars[ivar] = treevars[ivar];
+   //    // add training and test events; here: first half is training, second is testing
+   //    // note that the weight can also be event-wise
+   //    if (i < background->GetEntries()/2) dataloader->AddBackgroundTrainingEvent( vars, backgroundWeight*weight );
+   //    else                                dataloader->AddBackgroundTestEvent    ( vars, backgroundWeight*weight );
+   // }
+   // // --- end ------------------------------------------------------------
+   // ```
+   // End of tree registration
+
+   // Set individual event weights (the variables must exist in the original TTree)
+   // -  for signal    : `dataloader->SetSignalWeightExpression    ("weight1*weight2");`
+   // -  for background: `dataloader->SetBackgroundWeightExpression("weight1*weight2");`
+//   dataloader->SetBackgroundWeightExpression( "weight" );
+
+
+   // Apply additional cuts on the signal and background samples (can be different)
+   TCut mycuts = "gmu1gmid>0 && gmu2gmid>0 && chi2LocMom>0 && chi2LocPos>0 && segComp>0  ";
+//&& 0<trkRelChi2<1.2 && 0<kinkFinder<30 && 0<glbKinkFinder<50 && 0<NTrkVHits<35 && 0<valPixHits<8 && 0<LWH<20 && 0<vMuonHitComb<35 && 0<segComp<1.2 && 0<timeAtIpInOutErr<3.5 && 0<innerChi2<3 && 0<outerChi2<4 && 0.7<trkValidFract<1 "; // for example: TCut mycuts = "abs(var1)<0.5 && abs(var2-0.5)<1";
+   TCut mycutb = "chi2LocMom>0 && chi2LocPos>0 && segComp>0";
+//"gmu1gmid<1 && gmu2gmid<1";
+//"chi2LocMom>0 && chi2LocMom<30 && chi2LocPos>0 && chi2LocPos<15 && segComp>0 ";
+//&& 0<trkRelChi2<1.2 && 0<kinkFinder<30 && 0<glbKinkFinder<50 && 0<NTrkVHits<35 && 0<valPixHits<8 && 0<LWH<20 && 0<vMuonHitComb<35 && 0<segComp<1.2 && 0<timeAtIpInOutErr<3.5 && 0<innerChi2<3 && 0<outerChi2<4 && 0.7<trkValidFract<1"; // for example: TCut mycutb = "abs(var1)<0.5";
+
+   // Tell the dataloader how to use the training and testing events
+   //
+   // If no numbers of events are given, half of the events in the tree are used
+   // for training, and the other half for testing:
+   //
+//       dataloader->PrepareTrainingAndTestTree( mycuts, mycutb, "SplitMode=random:!V" );
+   //
+   // To also specify the number of testing events, use:
+   //
+   //    dataloader->PrepareTrainingAndTestTree( mycut,
+   //         "NSigTrain=3000:NBkgTrain=3000:NSigTest=3000:NBkgTest=3000:SplitMode=Random:!V" );
+   dataloader->PrepareTrainingAndTestTree( mycuts, mycutb,
+                                        "nTrain_Signal=400:SplitSeed=<Random number>:NormMode=NumEvents:!V" );
+
+   // ### Book MVA methods
+   //
+   // Please lookup the various method configuration options in the corresponding cxx files, eg:
+   // src/MethoCuts.cxx, etc, or here: http://tmva.sourceforge.net/optionRef.html
+   // it is possible to preset ranges in the option string in which the cut optimisation should be done:
+   // "...:CutRangeMin[2]=-1:CutRangeMax[2]=1"...", where [2] is the third input variable
+
+
+   // Boosted Decision Trees
+   if (Use["BDTG"]) // Gradient Boost
+      factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTG",
+                           "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:UseBaggedBoost:BaggedSampleFraction=0.5:nCuts=20:MaxDepth=2" );
+
+   if (Use["BDT"])  // Adaptive Boost
+      factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDT",
+                           "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" );
+
+   if (Use["BDTB"]) // Bagging
+      factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTB",
+                           "!H:!V:NTrees=400:BoostType=Bagging:SeparationType=GiniIndex:nCuts=20" );
+
+   // For an example of the category classifier usage, see: TMVAClassificationCategory
+   //
+   // --------------------------------------------------------------------------------------------------
+   //  Now you can optimize the setting (configuration) of the MVAs using the set of training events
+   // STILL EXPERIMENTAL and only implemented for BDT's !
+   //
+   //     factory->OptimizeAllMethods("SigEffAt001","Scan");
+   //     factory->OptimizeAllMethods("ROCIntegral","FitGA");
+   //
+   // --------------------------------------------------------------------------------------------------
+
+   // Now you can tell the factory to train, test, and evaluate the MVAs
+   //
+   // Train MVAs using the set of training events
+   factory->TrainAllMethods();
+
+   // Evaluate all MVAs using the set of test events
+   factory->TestAllMethods();
+
+   // Evaluate and compare performance of all configured MVAs
+   factory->EvaluateAllMethods();
+
+   // --------------------------------------------------------------
+
+   // Save the output
+   outputFile->Close();
+
+   std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
+   std::cout << "==> TMVAClassification is done!" << std::endl;
+
+   delete factory;
+   delete dataloader;
+   // Launch the GUI for the root macros
+   if (!gROOT->IsBatch()) TMVA::TMVAGui( outfileName );
+
+   return 0;
+}
+
+int main( int argc, char** argv )
+{
+   // Select methods (don't look at this code - not of interest)
+   TString methodList;
+   for (int i=1; i<argc; i++) {
+      TString regMethod(argv[i]);
+      if(regMethod=="-b" || regMethod=="--batch") continue;
+      if (!methodList.IsNull()) methodList += TString(",");
+      methodList += regMethod;
+   }
+   return TMVAClassification(methodList);
+}
